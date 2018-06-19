6f48ad514c098fceb3faa6f7d3085713c0f0a810
Katarzyna Kruczkowska <kkruczkowska@gmail.com>
2018-06-19 22:58:40 +0200

    19-06-2018 całkiem nowa odsłona

--- src/com/company/GameManager.java
+++ src/com/company/GameManager.java
@@ -19,10 +19,20 @@ public class GameManager {
     private static final String FORMATTED_PROVIDE_NAME = "%s %d %s \n";
     private static final String FORMATED_WELCOME = "%s %s %s %s \n";
 
-    public static void play() {
+    public void play() {
 
-        boolean shouldPlayAgain = true;
         final Player players[] = new Player[NUMBER_OF_PLAYERS];
+        initPlayers(players);
+        playGame(players);
+        ioManager.showMessage(END_OF_THE_GAME);
+    }
+
+    public static int getRandomPlayerIndex(final int playerCount) {
+        final Random random = new Random();
+        return random.nextInt(playerCount);
+    }
+
+    public void initPlayers(Player players[]) {
         final PlayerSignType sign[] = new PlayerSignType[NUMBER_OF_PLAYERS];
 
         if (getRandomPlayerIndex(NUMBER_OF_PLAYERS) == 0) {
@@ -36,58 +46,44 @@ public class GameManager {
         for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
             ioManager.showMessage(format(FORMATTED_PROVIDE_NAME, PLAYER, i + 1, PROVIDE_YOUR_NAME));
             String name = ioManager.getName();
-
             players[i] = new Player(name, sign[i]);
-
             ioManager.showMessage(format(FORMATED_WELCOME, HELLO, players[i].getName(), PLAYING_AS, players[i].getSign()));
-
         }
+    }
 
+    public void playGame(Player players[]) {
+        boolean shouldPlayAgain = true;
         final int boardSize = ioManager.getBoardSize(MIN_BOARD_SIZE, MAX_BOARD_SIZE);
         ioManager.showMessage(format(FORMATED_SELECT, SELECTED, boardSize));
         final Board board = new Board(boardSize);
-
-        Player currentPlayer = players[0];
-        if (getRandomPlayerIndex(NUMBER_OF_PLAYERS) == 1) {
-            currentPlayer = players[1];
-        }
+        int activePlayerId = getRandomPlayerIndex(NUMBER_OF_PLAYERS);
 
         ioManager.showMessage(DRAWN_PLAYER);
-        ioManager.showMessage(format(FORMATED_PLAYER, currentPlayer.getName(), currentPlayer.getSign()));
+        ioManager.showMessage(format(FORMATED_PLAYER, players[activePlayerId].getName(), players[activePlayerId].getSign()));
 
         do {
             ioManager.showBoard(board);
-            ioManager.showMessage(format(FORMATED_PLAYER, currentPlayer.getName(), currentPlayer.getSign()));
+            ioManager.showMessage(format(FORMATED_PLAYER, players[activePlayerId].getName(), players[activePlayerId].getSign()));
 
             final Coordinates coordinates = ioManager.getCoordinates(MIN_BOARD_ID, boardSize);
 
-            System.out.printf(FORMATED_CHOICE, SELECTED, ROW, coordinates.row + 1, COLUMN, coordinates.column + 1);
+            ioManager.showMessage(format(FORMATED_CHOICE, SELECTED, ROW, coordinates.row + 1, COLUMN, coordinates.column + 1));
 
-            if (board.insertSign(currentPlayer.getSign(), coordinates.row, coordinates.column)) {
+            if (board.insertSign(players[activePlayerId].getSign(), coordinates.row, coordinates.column)) {
                 board.addSignValue(coordinates.row, coordinates.column);
                 if (board.checkWinner(coordinates.row, coordinates.column)) {
                     ioManager.showBoard(board);
-                    ioManager.showMessage(format(FORMATED_WINNER, WINNER, currentPlayer.getName(), currentPlayer.getSign()));
+                    ioManager.showMessage(format(FORMATED_WINNER, WINNER, players[activePlayerId].getName(), players[activePlayerId].getSign()));
                     shouldPlayAgain = false;
                 }
                 if (board.getCountOfEmptyField() == 0) {
                     ioManager.showBoard(board);
                     shouldPlayAgain = false;
                 }
-                if (currentPlayer == players[0]) {
-                    currentPlayer = players[1];
-                } else {
-                    currentPlayer = players[0];
-                }
+                activePlayerId = ++activePlayerId % NUMBER_OF_PLAYERS;
             } else {
                 ioManager.showMessage(NOT_EMPTY_PLACE);
             }
         } while (shouldPlayAgain);
-        ioManager.showMessage(END_OF_THE_GAME);
-    }
-
-    public static int getRandomPlayerIndex(final int playerCount) {
-        final Random random = new Random();
-        return random.nextInt(playerCount);
     }
 }
